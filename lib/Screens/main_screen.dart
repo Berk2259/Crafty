db0568ed6856d888f47fab41c84e6c32ef69a969
Berk Altay <berkaltay3435@gmail.com>
2025-09-15 10:50:55 +0300

    curvedBar kurulumu

---
+++ lib/Screens/main_screen.dart
@@ -0,0 +1,37 @@
+import 'package:crafty/Screens/screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<Widget> pages = [HomeScreen(), ProfileScreen()];
+  final List<Widget> icons = [
+    Image.asset('assets/images/home.png'),
+    Image.asset('assets/images/profile.png'),
+  ];
+  int _page = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_page],
+      bottomNavigationBar: CurvedNavigationBar(
+        items: icons,
+        index: _page,
+        color: Colors.purple,
+        animationDuration: Duration(milliseconds: 300),
+        backgroundColor: Colors.transparent,
+        height: 65,
+        onTap:
+            (value) => setState(() {
+              _page = value;
+            }),
+      ),
+    );
+  }
+}
